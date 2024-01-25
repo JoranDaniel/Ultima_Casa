47cf10d8568cc94b77fe0948de7ae3d32f7a1b54
Joran Daniël <124999321+JoranDaniel@users.noreply.github.com>
2024-01-25 10:49:36 +0100

    test commit

--- Versie 1.00/Bronbestanden/admin.php
+++ Versie 1.00/Bronbestanden/admin.php
@@ -51,111 +51,6 @@
                   FROM statussen
               ORDER BY StatusCode";
 
-<<<<<<< Updated upstream
-     $records = $db->query($sql)->fetchAll();    
-                         
-     echo               '<div id="statussen" class="tab-pane fade in active">
-                              <h3>Statussen</h3>
-                              <table id="table_statussen">
-                                   <tr>
-                                        <th>Status</th>
-                                        <th>StatusCode</th>
-                                        <th>&nbsp;</th>
-                                        <th>&nbsp;</th>
-                                        <th class="button-column">
-                                             <form action="statusplus.php">                             
-                                                  <button type="submit" class="action-button"
-                                                          title="Een nieuwe status toevoegen.">&nbsp;+&nbsp;
-                                                  </button>
-                                                  <input type="hidden" value="' . $relatieid . '" id="RID" name="RID">
-                                             </form>
-                                        </th>
-                                   </tr>';
-     foreach ($records as $record) 
-     {    $ID = $record['ID'];
-          $sql = "SELECT StartDatum, 
-                         CONCAT(Straat, ', ', LEFT(Postcode, 4), ' ', RIGHT(Postcode, 2), '&nbsp;&nbsp;', Plaats) as Adres
-                    FROM biedingen
-               LEFT JOIN huizen ON huizen.ID = biedingen.FKhuizenID 
-                   WHERE biedingen.FKstatussenID = $ID
-                GROUP BY biedingen.FKhuizenID
-                ORDER BY StartDatum";      
-           
-          $statussen = $db->query($sql)->fetchAll();    
-          $disabled = "";
-          $niet = "Deze status verwijderen.";
-          if ($statussen)
-          {    $disabled = " disabled";
-               $niet = "Deze status mag NIET verwijderd worden.";
-          }
-          echo                    '<tr>
-                                        <td>' . $record['Status'] . '</td>
-                                        <td>' . $record['StatusCode'] . '</td>
-                                        <td class="button-column">
-                                             <form action="statusedit.php">                             
-                                                  <button type="submit" class="action-button" id="edit" name="edit" 
-                                                          value="' . $record['ID'] . '" title="Deze status wijzigen.">...
-                                                  </button>
-                                                  <input type="hidden" value="' . $relatieid . '" id="RID" name="RID">
-                                             </form>
-                                        </td>
-                                        <td class="button-column">
-                                             <form action="statusmin.php">                             
-                                                  <button type="submit" class="action-button" id="wis" name="wis"' . $disabled . ' 
-                                                          value="' . $record['ID'] . '" title="' . $niet . '">&nbsp;-&nbsp;
-                                                  </button>
-                                                  <input type="hidden" value="' . $relatieid . '" id="RID" name="RID">
-                                             </form>
-                                        </td>
-                                        <td class="button-column">
-                                            <button type="button" class="action-button" title="Huizen met deze status." 
-                                                    data-toggle="collapse" data-target="#huizen_' . $record['ID'] . '">&nbsp;&#9660;&nbsp;
-                                            </button>
-                                       </td>
-                                   </tr>
-                                   <tr>
-                                        <td colspan=5 >
-                                             <div id="huizen_' . $record['ID'] . '" class="collapse">
-                                                  <table id="table_huizen" class="details">
-                                                       <tr>';
-          if (!$statussen)
-          {    echo                                        '<th colspan=4 class="cell-center">
-                                                                 <h4>Er zijn geen huizen met status ' . $record["StatusCode"] . ' - ' . $record["Status"] . '</h4>
-                                                           </th>';
-          }
-          else
-          {    echo                                        '<th colspan=4 class="cell-center">
-                                                                 <h4>Huizen met status ' . $record["StatusCode"] . ' - ' . $record["Status"] . '</h4>
-                                                           </th>
-                                                       </tr>
-                                                       <tr>
-                                                            <th>&nbsp;</th>
-                                                            <th>Adres</th>
-                                                            <th>Te koop sinds</th>
-                                                            <th>&nbsp;</th>
-                                                       </tr>';
-               foreach ($statussen as $record) 
-               {    echo                              '<tr>
-                                                            <td>&nbsp;</td>                                             
-                                                            <td>' . $record['Adres'] . '</td>                                             
-                                                            <td>' . $record['StartDatum'] . '</td>
-                                                            <td>&nbsp;</td>                                             
-                                                       </tr>';
-               }
-          };
-          echo                                   '</table>
-                                             
-                                             </div>
-                                        </td>
-                                   </tr>';    
-     }
-     echo                    '</table>
-                         </div>';
-
-     $sql = "   SELECT ID, Naam, Omschrijving, Waarde, Landingspagina
-                  FROM rollen
-              ORDER BY Waarde";
-=======
 // Check if the user is logged in
 if (!isset($_SESSION['RID'])) {
     // Redirect to the login page if not logged in
@@ -169,7 +64,6 @@ if ($_SESSION['RID'] != 529) {
     header("Location: unauthorized.php");
     exit();
 }
->>>>>>> Stashed changes
 
      $records = $db->query($sql)->fetchAll();    
                          
@@ -282,44 +176,6 @@ if ($_SESSION['RID'] != 529) {
              LEFT JOIN rollen ON rollen.ID = relaties.FKrollenID
               ORDER BY rollen.Waarde DESC, Naam";
 
-<<<<<<< Updated upstream
-     $records = $db->query($sql)->fetchAll();    
-                         
-     echo               '<div id="relaties" class="tab-pane fade">
-                              <h3>Accounts</h3>
-                              <table id="table_relaties">
-                                   <tr>
-                                        <th>Naam</th>
-                                        <th>Email</th>
-                                        <th>Telefoon</th>
-                                        <th>Rol</th>
-                                        <th>&nbsp;</th>
-                                   </tr>';
-     foreach ($records as $record) 
-     {    echo                    '<tr>
-                                        <td>' . $record['Naam'] . '</td>
-                                        <td>' . $record['Email'] . '</td>
-                                        <td>' . $record['Telefoon'] . '</td>                                             
-                                        <td>' . $record['Omschrijving'] . '</td>                                             
-                                        <td>
-                                             <form action="relatieedit.php">  
-                                                  <button type="submit" class="action-button" id="rel" name="rel" 
-                                                          value="' . $record['RID'] . '" title="De rol van dit account wijzigen.">&#x263A;
-                                                  </button>
-                                                  <input type="hidden" value="' . $relatieid . '" id="RID" name="RID">
-                                             </form>
-                                       </td>   
-                                   </tr>';
-     }
-     
-     echo                    '</table>
-                         </div>
-                    </div>
-               </div>
-          </body>
-     </html>';
-?>
-=======
 // Check if the accessed RID matches the logged-in user's ID and is in the database
 $allowed_ids = array(529);
 $relatieid = $_GET['RID'];
@@ -403,4 +259,3 @@ if ($relatieid != 529) {
      header("Location: makelaar.php");
      exit();
 }
->>>>>>> Stashed changes
